35d20474057d4c887f02d6fdd9fbe842657f25dd
Ariyaman Debnath <ariyamandebnath.ad@gmail.com>
2024-09-03 14:20:54 +0530

    feat: Add SignIn component with styled elements
    - Introduced `SignIn` component to handle user sign-in functionality.
    - Utilized `SignInContainer` for layout structure.
    - Added `LinkNextToSignIn` for sign-in link styling.
    - Wrapped `StyledButton` within an anchor tag for "Get Started" button.
    - Ensured consistent styling by importing components from `@/app/components/ui/layout/header/styled`.
    
    This commit enhances the user authentication flow by providing a styled sign-in interface.

--- packages/beaconbox_website/src/app/components/ui/layout/CallToAction.tsx
+++ packages/beaconbox_website/src/app/components/ui/layout/CallToAction.tsx
@@ -8,8 +8,8 @@ import {
 export const SignIn = () => {
     return (
         <SignInContainer>
-            <LinkNextToSignIn href='/'> Sign in</LinkNextToSignIn>
-            <a href="/">
+            <LinkNextToSignIn href='/api/auth/signin'> Sign in</LinkNextToSignIn>
+            <a href="/api/auth/signin">
                 <StyledButton>Get Started</StyledButton>
             </a>
         </SignInContainer>

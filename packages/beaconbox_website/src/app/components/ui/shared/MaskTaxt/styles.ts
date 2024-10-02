fbe42a9292f92a1d665b7c0da95818d5ed33cd67
Ariyaman Debnath <ariyamandebnath.ad@gmail.com>
2024-10-02 19:06:05 +0530

    feat: Add MaskText component with framer-motion animations
    - Implemented `MaskText` component for animating text reveals using `framer-motion`.
    - Created smooth transition for text lines with staggered animation controlled by scroll position.
    - Added support for various HTML tags (`h1`, `h2`, `h3`, `p`) to dynamically render based on the provided `tag` prop.
    - Used `useInView` hook to trigger animation once the component enters the viewport, ensuring animations only occur once.
    - Integrated `Body` and `LineMask` styled components for layout and masking of text lines.

---
+++ packages/beaconbox_website/src/app/components/ui/shared/MaskTaxt/styles.ts
@@ -0,0 +1,15 @@
+'use client';
+
+import  styled  from '@emotion/styled';
+
+export const Body = styled.div`
+    h1,
+    p{
+
+    }`;
+
+
+export const LineMask = styled.div`
+    overflow: hidden`
+    ;
+
